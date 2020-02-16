848a5ef72299d3159f5f1cdfd282f1ecd7c9d4e8
Jin Wu <jinn.wuu@gmail.com>
2020-02-16 22:56:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function writeCards( namesArray, event ) {
   }
   return thankYouCards
 }
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
