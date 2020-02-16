60c2e6a54bde2d27dc42c7123df759b3187f7587
Jin Wu <jinn.wuu@gmail.com>
2020-02-16 22:56:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // Code your solutions in this file
 
-writeCards()
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
